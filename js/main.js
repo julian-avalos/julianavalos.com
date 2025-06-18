372cdc7a1a7d836338efbcaccf20befbd9f0e69a
Julian Avalos <avalosju@Mac107816.local>
2025-06-18 14:05:16 -0700

    updated path in fetch fuction

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Blog loader (simple example, loads a list of markdown files)
 if (document.getElementById('blog-list')) {
-  fetch('/blog-posts/index.json')
+  fetch('/posts/index.json')
     .then(res => res.json())
     .then(posts => {
       const list = document.getElementById('blog-list');
       posts.forEach(post => {
         const div = document.createElement('div');
-        div.innerHTML = `<h2>${post.title}</h2><p>${post.date}</p><a href="/blog-posts/${post.file}">Read more</a>`;
+        div.innerHTML = `<h2>${post.title}</h2><p>${post.date}</p><a href="/posts/${post.file}">Read more</a>`;
         list.appendChild(div);
       });
     });
