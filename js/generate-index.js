ac91ac0421bdbd9a97c2e34fda6e898ce108c7ec
Julian Avalos <avalosju@Mac107816.local>
2025-06-18 15:26:09 -0700

    updated paths in generate-index.js and package.json

--- js/generate-index.js
+++ js/generate-index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const postsDir = path.join(__dirname, 'posts');
+const postsDir = path.join(__dirname, '../posts');
 const indexFile = path.join(postsDir, 'index.json');
 
 function extractFrontmatter(content) {
